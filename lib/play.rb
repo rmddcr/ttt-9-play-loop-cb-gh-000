3d3fa088bec3f478af862d6b231056da87dd7a41
Damitha Ranasingha <rmddcr@gmail.com>
2018-03-31 13:20:53 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,14 @@
 # Helper Methods
+def play(board)
+  turn = 0
+  until turn ==9
+    turn +=1
+    turn(board)
+  end
+end
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
